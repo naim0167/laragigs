1731017798c67cba141d1b9fc99e0f5511e2abef
Md. Naim Chowdhury <naimmcduck@gmail.com>
2022-12-30 19:05:50 +0600

    Job Portal

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Carbon\Factory;
+use App\Models\User;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,9 +20,20 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@gmail.com',
+
+        ]);
+
+
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Models\Listing;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +17,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ListingController::class, 'index']);
 
-Route::get('/hello', function(){
-    return response('<h1>Hello World</h1>');
-});
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
+
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
+
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
+
+Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
+
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
+
+Route::get('/listings/manage', [ListingController::class, 'manage'])->middleware('auth');
+
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+Route::get('/register', [UserController::class, 'create'])->middleware('guest') ;
+
+Route::post('/users', [UserController::class, 'store']);
+
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
+
+
